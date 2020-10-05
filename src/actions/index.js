dc679f895626f96df29fdf43cc589a2a4abd452e
Shawn Smith <srsmith11@live.com>
2020-10-05 16:35:20 -0600

    created custom history and CRU actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import { SIGN_IN, SIGN_OUT, CREATE_STREAM, FETCH_STREAMS, FETCH_STREAM, DELETE_STREAM, EDIT_STREAM } from './types';
 
 export const signIn = (userId) => {
@@ -19,6 +20,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 	const response = await streams.post('/streams', { ...formValues, userId });
 
 	dispatch({ type: CREATE_STREAM, payload: response.data });
+	history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
@@ -40,7 +42,8 @@ export const deleteStream = (id) => async (dispatch) => {
 };
 
 export const editStream = (id, formValues) => async (dispatch) => {
-	const response = await streams.put(`/streams/${id}`, formValues);
+	const response = await streams.patch(`/streams/${id}`, formValues);
 
 	dispatch({ type: EDIT_STREAM, payload: response.data });
+	history.push('/');
 };
