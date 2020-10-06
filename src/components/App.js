e2a9ff1e78c1b55c0dc606be41c5fa1bb4c755b6
Shawn Smith <srsmith11@live.com>
2020-10-06 10:06:07 -0600

    added delete feature

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import { Router, Route } from 'react-router-dom';
-import StreamList from './streams/StreamList';
-import StreamCreate from './streams/StreamCreate';
-import StreamDelete from './streams/StreamDelete';
-import StreamShow from './streams/StreamShow';
-import StreamEdit from './streams/StreamEdit';
-import Header from './Header';
-import history from '../history';
+import React from "react";
+import { Router, Route } from "react-router-dom";
+import StreamList from "./streams/StreamList";
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamShow from "./streams/StreamShow";
+import StreamEdit from "./streams/StreamEdit";
+import Header from "./Header";
+import history from "../history";
 
 const App = () => {
-	return (
-		<div className="ui container">
-			<Router history={history}>
-				<div>
-					<Header />
-					<Route path="/" exact component={StreamList} />
-					<Route path="/streams/new" component={StreamCreate} />
-					<Route path="/streams/edit/:id" component={StreamEdit} />
-					<Route path="/streams/delete" component={StreamDelete} />
-					<Route path="/streams/show" component={StreamShow} />
-				</div>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="ui container">
+      <Router history={history}>
+        <div>
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" component={StreamCreate} />
+          <Route path="/streams/edit/:id" component={StreamEdit} />
+          <Route path="/streams/delete/:id" component={StreamDelete} />
+          <Route path="/streams/show" component={StreamShow} />
+        </div>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
