a0c57366aff9334e4cc502deeb3556bd1cb1faac
Shawn Smith <srsmith11@live.com>
2020-10-06 13:59:19 -0600

    show page complete

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-const StreamShow = () => {
-	return <div>StreamShow</div>;
+//used a functional component instead of class just to try it
+const StreamShow = (props) => {
+  useEffect(() => {
+    props.fetchStream(props.match.params.id);
+  }, []);
+
+  if (!props.stream) {
+    return <div>Loading...</div>;
+  }
+
+  const { title, description } = props.stream;
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h5>{description}</h5>
+    </div>
+  );
 };
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
